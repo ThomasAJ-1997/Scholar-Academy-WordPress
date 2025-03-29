9e3c9514f37eefc04123584f9241ce28cc8ad165
Thomas Jones <Thomasajones@hotmail.co.uk>
2025-03-29 13:01:26 +0000

    clean code commit

--- functions.php
+++ functions.php
@@ -1,5 +1,35 @@
 <?php
 
+function page_banner($args = NULL) {
+    if (!isset($args['title'])) {
+        $args['title'] = get_the_title();
+      }
+
+      if (!isset($args['subtitle'])) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+      } 
+
+      if (!isset($args['photo'])) {
+        if (get_field('page_banner_background_image')) {
+          $args['photo'] = get_field('page_banner_background_image')['sizes']['page_banner'];
+        } else {
+          $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+    ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php 
+      echo $args['photo'] ?>"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+        <div class="page-banner__intro">
+          <p><?php echo $args['subtitle'] ?></p>
+        </div>
+      </div>
+    </div>
+
+<?php }
+
 function scholar_academy_files() {
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('academy_main_styles', get_theme_file_uri('/build/style-index.css'));
@@ -13,9 +43,6 @@ add_action('wp_enqueue_scripts', 'scholar_academy_files');
 // Second argument is the name of a function we want to run: we create this function.
 
 function scholar_academy_features() {
-    // register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    // register_nav_menu('footerLocationOne', 'Footer Location One');
-    // register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
     add_theme_support( 'post-thumbnails', array( 'professor' ));
     add_image_size('professor_landscape', 400, 260, true);
